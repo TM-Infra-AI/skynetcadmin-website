566e391d6f3a38070d9c4bf6ae47bf6191676bed
Ashu <ashu_sharma@technologymindz.com>
2026-04-27 19:00:46 +0530

    Initial commit for skynetcadmin

---
+++ themes/spacious/js/spacious-slider-setting.js
@@ -0,0 +1,1 @@
+jQuery(window).on('load',function(){jQuery('.slider-cycle').cycle({fx:'fade',timeout:4000,speed:1000,slides:'> div',pager:'> #controllers',pagerActiveClass:'active',pagerTemplate:'<a></a>',pauseOnHover:true,autoHeight:'container',swipe:true,swipeFx:'scrollHorz',log:false});});
